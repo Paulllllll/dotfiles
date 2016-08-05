a9e6dcd9c75626017230e85c4a4f4f477adb456a
Paulllllll <paulrchua@gmail.com>
2016-08-05 13:22:18 -0500

    Update paulvironment.sh

--- paulvironment.sh
+++ paulvironment.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install zsh
+sudo apt-get install vim
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo apt-get install tmux
